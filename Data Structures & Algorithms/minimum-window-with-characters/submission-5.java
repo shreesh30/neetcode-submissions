71f00a2b9f1d88674d550ef2ab4aec3d6027d2d4
Shreesh Arya <shreesharya20@gmail.com>
2026-08-09 01:27:48 +0530

    Add: minimum-window-with-characters - submission-5

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-5.java
@@ -0,0 +1,134 @@
+class Solution {
+    public String minWindow(String s, String t) {
+        /* BRUTE FORCE APPROACH:
+        TIME COMPLEXITY: O(N^2*K)~O(N^2)
+        SPACE COMPLEXITY: O(N)
+
+
+        int n=s.length();
+        int m=t.length();
+
+        Map<Character, Integer> freqT=new HashMap<>();
+
+        for(char c:t.toCharArray()){
+            freqT.put(c, freqT.getOrDefault(c, 0)+1);
+        }
+
+        int minLength=Integer.MAX_VALUE;
+
+        String result="";
+
+        for(int i=0;i<n;i++){
+            Map<Character, Integer> freqMap=new HashMap<>();
+
+            for(int j=i;j<n;j++){
+                char c=s.charAt(j);
+                freqMap.put(c,freqMap.getOrDefault(c,0)+1);
+
+                boolean valid=true;
+
+                for(Map.Entry<Character,Integer> entry:freqT.entrySet()){
+                    if(freqMap.getOrDefault(entry.getKey(),0)<entry.getValue()){
+                        valid=false;
+                        break;
+                    }
+
+                }
+                  if(valid && (j-i+1)<minLength){
+                        minLength=j-i+1;
+                        result=s.substring(i,j+1);
+                    }
+            }
+        }
+
+        return result;
+        */
+
+        /* SLIGHTLY OPTIMIZED:
+        TIME COMPLEXITY: O(N^2)
+        SPACE COMPLEXITY: O(N)
+
+        int n = s.length();
+        int m = t.length();
+
+        Map<Character, Integer> tFreq = new HashMap<>();
+
+        for (char c : t.toCharArray()) {
+            tFreq.put(c, tFreq.getOrDefault(c, 0) + 1);
+        }
+
+        String result = "";
+
+        for (int i = 0; i < n; i++) {
+            int required = m;
+            Map<Character, Integer> windowFreq=new HashMap<>();
+
+            for (int j = i; j < n; j++) {
+                char c = s.charAt(j);
+
+                windowFreq.put(c, windowFreq.getOrDefault(c,0)+1);
+
+                if (windowFreq.get(c)<=tFreq.getOrDefault(c,0)) {
+                    required -= 1;
+                }
+
+                if (required == 0 && (result.isBlank() || (j - i + 1) < result.length())) {
+                        result = s.substring(i, j + 1);
+                        break;
+                }
+            }
+        }
+
+        return result;
+        */
+
+        int m = s.length();
+        int n = t.length();
+
+        Map<Character, Integer> tFreq = new HashMap<>();
+
+        for (char c : t.toCharArray()) {
+            tFreq.put(c, tFreq.getOrDefault(c, 0) + 1);
+        }
+
+        int left = 0, right = 0;
+
+        Map<Character, Integer> windowFreq = new HashMap<>();
+        int required = n;
+
+        String result = "";
+        int start=-1,end=-1;
+
+        while (right < m) {
+            char c = s.charAt(right);
+            windowFreq.put(c, windowFreq.getOrDefault(c, 0) + 1);
+
+            if (windowFreq.get(c) <= tFreq.getOrDefault(c, 0)) {
+                required--;
+
+                while(required==0){
+                    if((start==-1)||(right-left+1)<(end-start+1)){
+                        start=left;
+                        end=right;
+                    }
+
+                    char leftChar=s.charAt(left);
+                    windowFreq.put(leftChar,
+                    windowFreq.get(leftChar)-1);
+
+                    if(windowFreq.get(leftChar)<tFreq.getOrDefault(leftChar,0)){
+                        required++;
+                    }
+                    left++;
+                }
+            }
+            right++;
+        }
+
+          if (start == -1) {
+            return "";
+        }
+
+        return s.substring(start, end + 1);
+    }
+}
